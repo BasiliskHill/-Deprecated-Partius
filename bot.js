0f7661269424c0228d319a3ef4ecf167565e8b77
Basilisk <33152005+BasiliskHill@users.noreply.github.com>
2017-12-25 21:41:29 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,5 @@
 const Eris = require("eris");
 var config = require("dev.json");
-console.log(config.commandOptions);
-console.log(require("dev.json").commandOptions);
 
 var bot = new Eris.CommandClient(config.token, {}, config.commandOptions);
 
