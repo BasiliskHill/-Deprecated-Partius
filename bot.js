c9d73eff48d758a1360b95148bc120ddb1ad1fe4
BasiliskHill <hunter@dragonflyhill.com.au>
2018-01-09 20:35:34 +0800

    Moved 'ping' over to modules

--- bot.js
+++ bot.js
@@ -25,12 +25,6 @@ fs.readdir("./commands", (err,files) => {
 	}
 });
 
-var ping = bot.registerCommand("ping", ["Pang!", "Peng!", "Pong!", "Pung!"], {
-	//Responds with random versions of "Ping!" when someone says "/pt pong"
-	description: "Pong!",
-	fullDescription: "A command to check if the bot is up, or for entertainment... Weirdos."
-})
-
 //Functions used
 var functions = {
 	serverLog: {
