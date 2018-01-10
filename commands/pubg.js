085043fae7befd1a573e042856a050bf525a0a7b
BasiliskHill <hunter@dragonflyhill.com.au>
2018-01-10 19:29:43 +0800

    Add pubg framework

---
+++ commands/pubg.js
@@ -0,0 +1,5 @@
+module.exports = {
+	register: function (bot, functions) {
+		bot.registerCommand("pubg", "Will be updated when the pubg-tracker API is back for testing", {});
+	}
+}
---
+++ commands/pubgsub.update.js
@@ -0,0 +1,7 @@
+module.exports = {
+	register: function (bot, functions) {
+		bot.commands.pubg.registerSubcommand("update", "Will be updated when the pubg-tracker API is back for testing", {
+			aliases: ["checkup", "check", "up"]
+		});
+	}
+}
