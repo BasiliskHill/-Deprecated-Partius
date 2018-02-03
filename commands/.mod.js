3bb3cdcff5c8e696d6621cfb1fa5eade4c28b338
BasiliskHill <hunter@dragonflyhill.com.au>
2018-02-03 16:12:26 +0800

    Made mod respond nicely

--- commands/.mod.js
+++ commands/.mod.js
@@ -1,7 +1,26 @@
 module.exports = {
     register: function(bot, functions) {
         bot.registerCommand("mod", (msg) => {
-            msg.createMessage("The mod parent command");
+            const date = new Date();
+            const commandList = []
+            
+            for (command in bot.commands.mod.subcommands) {
+                commandList.push(command);
+            }
+
+            msg.channel.createMessage({
+                embed: {
+                    title: "Partius moderation commands",
+                    description: `Command list: \n${msg.prefix}mod ${commandList.join(`\n${msg.prefix}mod `)}`,
+                    timestamp: date.toISOString(),
+				    color: 0xd50000,
+				    footer: {
+					    text: functions.footer(msg),
+					    icon_url: msg.author.avatarURL,
+				    },
+                }
+            });
+
         }, {
             usage: "<subCommand>",
             description: "The parent command for all your moderation needs",
